6744606b4649e194d2b814cdc8077343fd6430f7
Armslikenoodle <trevaskisdavid@gmail.com>
2024-04-30 21:52:18 +0100

    variable shown to user

--- index.php
+++ index.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Demo</title>
+    <style>
+      body {
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin:0;
+        font-family: sans-serif;
+      }
+    </style>
   </head>
   <body>
+    <?php 
+    $name = "Dark Matter";
+    ?>
     <h1>
-      <?php 
-      $greeting = "Hello";
-      echo "$greeting Everybody" 
-      ?>
+      You have read <?php echo $name; ?>
     </h1>
   </body>
 </html>
